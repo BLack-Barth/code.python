8d07dd1dc8d2a06aa1b3e5cc5b60165e5264ba00
Black Bart <mhamidat37@gmail.com>
2026-09-19 18:03:23 +0100

    Add files via upload

---
+++ code.py
@@ -0,0 +1,12 @@
+#rich | pyfiglet | keyword | range
+#list=[,] | sets={,} | tuples=(,) | dict={key:value,}
+# range(start,stop,step)
+# input -> str |  int(input) -> int | float(input) -> float
+# import getpass
+#password = getpass.getpass('Please enter psswd:')
+from rich import print
+
+if password == '08452200':
+    print("welcome black bart!")
+else:
+    print("Error!!!")
