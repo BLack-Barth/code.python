e00c55a2a854e443b49c6d97eac3114fea2b1f99
Black Bart <mhamidat37@gmail.com>
2026-09-26 22:05:41 +0100

    Fetch headers from JSONPlaceholder API
    Updated the code to fetch headers from a new URL and handle response status.

--- code.py
+++ code.py
@@ -8,13 +8,11 @@
 # https://jsonplaceholder.typicode.com/posts
 from rich import print
 import requests
-
-URL = 'https://jsonplaceholder.typicode.com/users'
-data = {
-    "userId" : 11,
-    "name" : "black bart",
-    "job" : "bug bounty hunter"
-}
-response = requests.post(URL,data)
-print(f'code status : {response.status_code}')
-print(response.json())
+url = 'https://jsonplaceholder.typicode.com/'
+response = requests.get(url)
+if response.status_code == 200 :
+    print("[bold red]Technology Enumeration :[/]")
+    for key,value in response.headers.items() :
+        print(f"[underline yellow]{key} [/]: [italic blue]{value} [/]")
+else :
+    print('[bold red] The server is down')
